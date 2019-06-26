d3c106c53e03f712b45069ff1848e0e1bc77b805
SteveK1964 <steve.kuplic@comcast.net>
2019-06-26 16:40:56 -0500

    sd

--- bash/loop.sh
+++ bash/loop.sh
@@ -10,4 +10,10 @@ do
   echo $COMMAND
 done
 
-./loop.sh
+# List all of the configuration files in the _/etc/apache2/sites-available/_ directory
+VHOSTS_PATH=/etc/apache2/sites-available/*.conf
+
+for FILENAME in $VHOSTS_PATH
+do
+  echo $FILENAME
+done
