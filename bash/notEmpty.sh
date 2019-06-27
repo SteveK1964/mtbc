558a2af0f3d03203b86f92862fc4b9c1e1c791bb
SteveK1964 <steve.kuplic@comcast.net>
2019-06-26 22:50:07 -0500

    New file notEmpty,sh to be committed

---
+++ bash/notEmpty.sh
@@ -0,0 +1,17 @@
+# notEmpty
+
+#!/bin/bash
+
+STRING=''
+while [ "$STRING" != "Hello World" ]
+do
+    if [ -z  "$STRING" ]
+    then
+      STRING="Hello"
+    else
+      STRING="${STRING} World"
+    fi
+
+    echo "$STRING"
+    
+done
